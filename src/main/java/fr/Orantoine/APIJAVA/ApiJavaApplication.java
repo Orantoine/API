c9515054390643eed6ac00452ea5534cb5be9e0e
Antoine Godeau <antoine.godeau@sfr.fr>
2019-01-26 17:22:13 +0100

    Creation du projet

---
+++ src/main/java/fr/Orantoine/APIJAVA/ApiJavaApplication.java
@@ -0,0 +1,14 @@
+package fr.Orantoine.APIJAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaApplication.class, args);
+	}
+
+}
+
